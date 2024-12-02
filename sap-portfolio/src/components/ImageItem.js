90fdff676efe5dd793aec0cc4097181b5c0bd051
msnkr <mikyle.designs@gmail.com>
2024-12-02 12:01:21 +0200

    .

--- sap-portfolio/src/components/ImageItem.js
+++ sap-portfolio/src/components/ImageItem.js
@@ -5,13 +5,12 @@ const ImageItem = ({ image, brand, script }) => {
         <img
           src={image}
           className="w-full h-[200px] md:h-[300px] lg:h-[500px]"
+          alt={`${brand}-logo`}
         />
       </div>
       <div>
         <div className="absolute top-1/2 -translate-y-1/2 left-12 text-white text-2xl">
-          <a>
-            <p className="cursor-pointer">{brand}</p>
-          </a>
+          <p className="cursor-pointer">{brand}</p>
           <p>{script}</p>
         </div>
       </div>
