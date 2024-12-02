e6b6c2bc64e12a6257019b45ee66d85b88d0971c
msnkr <mikyle.designs@gmail.com>
2024-12-02 08:03:05 +0200

    .

--- sap-portfolio/src/components/Navbar.js
+++ sap-portfolio/src/components/Navbar.js
@@ -3,12 +3,12 @@ import hamburgerIcon from "../images/icon-hamburger.svg";
 
 const Navbar = () => {
   return (
-    <div>
+    <div className="flex items-center justify-between">
       <div>
         <Logo />
       </div>
       <div>
-        <img src={hamburgerIcon} />
+        <img src={hamburgerIcon} className="w-10" />
       </div>
     </div>
   );
