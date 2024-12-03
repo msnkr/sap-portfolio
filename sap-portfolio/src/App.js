4bbe9a3aa63212b318a0ea38c8ef90c8aae60ce1
msnkr <mikyle.designs@gmail.com>
2024-12-03 12:16:08 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -7,10 +7,12 @@ import bos2 from "./images/home-page-image/bos-2.png";
 import newPlanet1 from "./images/home-page-image/new-planet-1.png";
 import newBeing1 from "./images/home-page-image/new-being-1.png";
 import tonic1 from "./images/home-page-image/tonic-1.png";
+import csa1 from "./images/home-page-image/csa-1.png";
+import entelect1 from "./images/home-page-image/entelect-1.png";
 
 export default function App() {
   return (
-    <div>
+    <div className="">
       <div className="navbar relative px-10 py-8">
         <Navbar />
       </div>
@@ -19,7 +21,7 @@ export default function App() {
           <p className="w-9/12 text-4xl font-bold tracking-wider leading-8 px-10">
             Hi. <br /> I'm Kashia Rees.
           </p>
-          <p className="px-10 text-lg tracking-tighter leading-6 font-custom">
+          <p className="px-10 text-lg tracking-tighter leading-6 font-custom md:w-8/12">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
           </p>
@@ -33,18 +35,41 @@ export default function App() {
           </div>
         </div>
       </div>
-      <div className="work-section pt-8">
+      <div className="work-section pt-8 ">
         <p className="px-10 text-2xl font-custom mb-4">Write</p>
-        <p className="px-10 tracking-tighter leading-6 font-custom mb-8">
+        <p className="px-10 tracking-tighter leading-6 font-custom mb-8 md:w-9/12">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
           aliquip ex ea commodo consequat.
         </p>
-        <ImageItem image={newPlanet1} script={"New Planet"} />
-        <ImageItem image={tonic1} script={"Tonic"} />
-        <ImageItem image={newBeing1} script={"Noobeing"} />
-        <ImageItem image={bos1} script={"MAKE-IT-BOS"} />
+        <p className="px-8 text-2xl font-custom mb-2">Previous work</p>
+        <div className="md:grid grid-cols-2 lg:grid-cols-3">
+          <ImageItem image={newPlanet1} script={"New Planet"} />
+          <ImageItem image={tonic1} script={"Tonic"} />
+          <ImageItem image={newBeing1} script={"Noobeing"} />
+          <ImageItem image={bos1} script={"MAKE-IT-BOS"} />
+          <ImageItem image={entelect1} script={"Entelect"} />
+          <ImageItem image={csa1} script={"Cricket SA"} />
+        </div>
+      </div>
+      <div>
+        <div>
+          <p className="px-10 text-2xl font-custom mb-4 mt-12">About</p>
+          <p className="px-10 tracking-tighter leading-6 font-custom md:w-9/12">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </p>
+        </div>
+        <div className="p-12 text-center">
+          <Footer />
+        </div>
       </div>
     </div>
   );
--- sap-portfolio/src/components/Footer.js
+++ sap-portfolio/src/components/Footer.js
@@ -3,6 +3,41 @@ import Logo from "./Logo";
 import linkedinIcon from "../images/icons-linkedin.svg";
 import behanceIcon from "../images/icon-behance.svg";
 
-const Footer = () => {};
+const Footer = () => {
+  return (
+    <div className="lg:flex justify-between items-center">
+      <div className="block lg:hidden">
+        <Logo />
+      </div>
+      <div>
+        <ul className="text-center space-y-1 font-custom tracking-tight">
+          <li>Tonic</li>
+          <li>Noobeing</li>
+          <li>BOS</li>
+          <li>New Planet</li>
+        </ul>
+      </div>
+      <div className="hidden lg:block">
+        <Logo />
+      </div>
+      <div>
+        <div className="space-y-6 lg:space-y-2">
+          <p className="text-xl font-custom tracking-tight">
+            someone@gmail.com
+          </p>
+          <div>
+            <button className="border-2 border-black py-1 px-4 font-semibold scale-95 hover:scale-100 duration-300">
+              Resume
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-center mt-6 lg:mt-2 space-x-6">
+          <img src={linkedinIcon} className="cursor-pointer" />
+          <img src={behanceIcon} className="cursor-pointer" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
