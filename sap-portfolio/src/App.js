e84a3fa2d7acc9652c5d5fe53d8ba9ea6f47cff5
msnkr <mikyle.designs@gmail.com>
2024-12-03 15:59:38 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -22,10 +22,10 @@ import heyHalfwayVideo from "./images/home-page-image/HH_Stache It (4x5).mp4";
 export default function App() {
   return (
     <div className="">
-      <div className="navbar relative px-8 py-12 lg:px-20">
+      <div className="navbar relative px-8 py-12 lg:px-20 xl:px-80">
         <Navbar />
       </div>
-      <div className="hero-section mt-20">
+      <div className="hero-section mt-20 xl:px-80">
         <div className="lg:grid grid-cols-2 space-y-12">
           <div className="lg:flex flex-col justify-center lg:ml-12 lg:mt-12 space-y-8">
             <p className="text-4xl font-bold tracking-wider leading-8 px-10">
@@ -41,7 +41,7 @@ export default function App() {
           </div>
         </div>
       </div>
-      <div className="work-section lg:mt-20">
+      <div className="work-section lg:mt-20 xl:px-80">
         <div className="lg:grid grid-cols-2">
           <div className="hidden lg:block">
             <ImageItem image={newPlanet1} />
@@ -73,7 +73,7 @@ export default function App() {
         </div>
       </div>
       <div>
-        <div className="mt-20 px-10 lg:px-20">
+        <div className="mt-20 px-10 lg:px-20 xl:px-80">
           <p className="text-2xl font-custom">About</p>
           <p className="tracking-tighter leading-6 font-custom mt-4">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
@@ -84,10 +84,26 @@ export default function App() {
             labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
             exercitation ullamco laboris nisi ut aliquip ex ea commodo
             consequat.
+            <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+            ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
           </p>
         </div>
         <div className="mt-20 flex justify-center">
           <VideoPlayer videoSrc={heyHalfwayVideo} />
+        </div>
+        <div className="hidden lg:grid grid-cols-2 lg:grid-cols-3 mt-4 xl:mt-40 lg:gap-4 px-80">
+          <ImageItem image={newPlanet2} script={"New Planet"} />
+          <ImageItem image={newPlanet3} script={"Tonic"} />
+          <ImageItem image={newBeing2} script={"Noobeing"} />
+          <ImageItem image={aynjil1} script={"Anyjil"} />
+          <ImageItem image={entelect2} script={"Entelect"} />
+          <ImageItem image={tonic2} script={"Tonic"} />
         </div>
         <div className="text-center mt-12">
           <Footer />
