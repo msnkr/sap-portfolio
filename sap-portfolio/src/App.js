9631c38f2637e3a34f3f4264fd14fb2aebf0713a
msnkr <mikyle.designs@gmail.com>
2024-12-03 10:02:14 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -3,6 +3,7 @@ import ImageItem from "./components/ImageItem";
 import Footer from "./components/Footer";
 
 import bos from "./images/home-page-image/bos-1.jpeg";
+import bosLogo from "./images/logo/bos-logo.png";
 
 export default function App() {
   return (
@@ -20,8 +21,9 @@ export default function App() {
             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
           </p>
           <div className="relative">
-            <div className="absolute top-1/2 -translate-y-1/2 left-4 text-white text-4xl z-50 font-bold">
+            <div className="absolute bottom-4 left-4 text-white text-4xl z-50 font-bold">
               <p>MAKE-IT-BOS</p>
+              <img src={bosLogo} className="w-12" />
             </div>
             <div className="bg-black w-full h-64 absolute top-0 left-0 opacity-25"></div>
             <img src={bos} className="w-full h-64 object-cover" />
