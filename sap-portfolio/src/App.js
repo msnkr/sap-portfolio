ca426e730b7ae9c3c483cf253aa2e7860d99972a
msnkr <mikyle.designs@gmail.com>
2024-12-03 11:40:31 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -2,8 +2,11 @@ import Navbar from "./components/Navbar";
 import ImageItem from "./components/ImageItem";
 import Footer from "./components/Footer";
 
-import bos from "./images/home-page-image/bos-1.jpeg";
-import bosLogo from "./images/logo/bos-logo.png";
+import bos1 from "./images/home-page-image/bos-1.jpeg";
+import bos2 from "./images/home-page-image/bos-2.png";
+import newPlanet1 from "./images/home-page-image/new-planet-1.png";
+import newBeing1 from "./images/home-page-image/new-being-1.png";
+import tonic1 from "./images/home-page-image/tonic-1.png";
 
 export default function App() {
   return (
@@ -12,7 +15,7 @@ export default function App() {
         <Navbar />
       </div>
       <div className="hero-section">
-        <div className="space-y-8">
+        <div className="space-y-12">
           <p className="w-9/12 text-4xl font-bold tracking-wider leading-8 px-10">
             Hi. <br /> I'm Kashia Rees.
           </p>
@@ -21,13 +24,9 @@ export default function App() {
             eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
           </p>
           <div className="relative">
-            <div className="absolute bottom-4 left-4 text-white text-4xl z-50 font-bold">
-              <p>MAKE-IT-BOS</p>
-              <img src={bosLogo} className="w-12" alt="BOS-logo" />
-            </div>
             <div className="bg-black w-full h-64 absolute top-0 left-0 opacity-25"></div>
             <img
-              src={bos}
+              src={bos2}
               className="w-full h-64 object-cover"
               alt="hero-image"
             />
@@ -35,15 +34,17 @@ export default function App() {
         </div>
       </div>
       <div className="work-section pt-8">
-        <p className="px-10">
+        <p className="px-10 text-2xl font-custom mb-4">Write</p>
+        <p className="px-10 tracking-tighter leading-6 font-custom mb-8">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
           minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          aliquip ex ea commodo consequat.
         </p>
+        <ImageItem image={newPlanet1} script={"New Planet"} />
+        <ImageItem image={tonic1} script={"Tonic"} />
+        <ImageItem image={newBeing1} script={"Noobeing"} />
+        <ImageItem image={bos1} script={"MAKE-IT-BOS"} />
       </div>
     </div>
   );
--- sap-portfolio/src/components/ImageItem.js
+++ sap-portfolio/src/components/ImageItem.js
@@ -1,16 +1,15 @@
-const ImageItem = ({ logo, script, image }) => {
+const ImageItem = ({ script, image }) => {
   return (
     <div>
       <div className="relative">
         <img
-          className="w-full h-96 object-cover"
+          className="w-full h-80 object-cover"
           src={image}
           alt={`${script}-image`}
         />
-        <div className="bg-black w-full h-96 absolute top-0 left-0 opacity-30"></div>
+        <div className="bg-black w-full h-80 absolute top-0 left-0 opacity-30"></div>
         <div className="absolute bottom-4 left-4">
           <p className="text-white font-bold text-4xl">{script}</p>
-          <img className="w-12" src={logo} alt={`${script}-logo`} />
         </div>
       </div>
     </div>
