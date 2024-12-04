25daeac30229ea54efeef40ed53a142fa7b638c0
msnkr <mikyle.designs@gmail.com>
2024-12-04 07:33:54 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -1,5 +1,4 @@
 import Navbar from "./components/Navbar";
-import ImageItem from "./components/ImageItem";
 import Footer from "./components/Footer";
 import VideoPlayer from "./components/VideoPlayer";
 
@@ -22,92 +21,11 @@ import heyHalfwayVideo from "./images/home-page-image/HH_Stache It (4x5).mp4";
 export default function App() {
   return (
     <div className="">
-      <div className="navbar relative px-8 py-12 lg:px-20 xl:px-80">
+      <div className="navbar relative px-8 py-12">
         <Navbar />
       </div>
-      <div className="hero-section mt-20 xl:px-80">
-        <div className="lg:grid grid-cols-2 space-y-12">
-          <div className="lg:flex flex-col justify-center lg:ml-12 lg:mt-12 space-y-8">
-            <p className="text-4xl font-bold tracking-wider leading-8 px-10">
-              Hi. <br /> I'm Kashia Rees.
-            </p>
-            <p className="text-lg tracking-tighter leading-6 font-custom px-10">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
-            </p>
-          </div>
-          <div className="relative">
-            <ImageItem image={bos2} />
-          </div>
-        </div>
-      </div>
-      <div className="work-section lg:mt-20 xl:px-80">
-        <div className="lg:grid grid-cols-2">
-          <div className="hidden lg:block">
-            <ImageItem image={newPlanet1} />
-          </div>
-          <div>
-            <p className="lg:flex flex-col justify-center text-2xl font-custom mt-20 lg:mt-0 px-10 lg:px-20">
-              Write
-            </p>
-            <p className="tracking-tighter leading-6 font-custom mt-4 px-10 lg:px-20">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit
-              amet, consectetur adipiscing elit, sed do eiusmod tempor
-              incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-              veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-              ex ea commodo consequat.
-            </p>
-          </div>
-        </div>
-        <p className="text-2xl font-custom mt-20 px-10">Previous work</p>
-        <div className="md:grid grid-cols-2 lg:grid-cols-3 mt-4 lg:gap-4">
-          <ImageItem image={newPlanet1} script={"New Planet"} />
-          <ImageItem image={tonic1} script={"Tonic"} />
-          <ImageItem image={newBeing1} script={"Noobeing"} />
-          <ImageItem image={bos1} script={"MAKE-IT-BOS"} />
-          <ImageItem image={entelect1} script={"Entelect"} />
-          <ImageItem image={csa1} script={"Cricket SA"} />
-        </div>
-      </div>
       <div>
-        <div className="mt-20 px-10 lg:px-20 xl:px-80">
-          <p className="text-2xl font-custom">About</p>
-          <p className="tracking-tighter leading-6 font-custom mt-4">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-            <br /> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
-            do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-            enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-            ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet,
-            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-            exercitation ullamco laboris nisi ut aliquip ex ea commodo
-            consequat.
-          </p>
-        </div>
-        <div className="mt-20 flex justify-center">
-          <VideoPlayer videoSrc={heyHalfwayVideo} />
-        </div>
-        <div className="hidden lg:grid grid-cols-2 lg:grid-cols-3 mt-4 xl:mt-40 lg:gap-4 px-80">
-          <ImageItem image={newPlanet2} script={"New Planet"} />
-          <ImageItem image={newPlanet3} script={"Tonic"} />
-          <ImageItem image={newBeing2} script={"Noobeing"} />
-          <ImageItem image={aynjil1} script={"Anyjil"} />
-          <ImageItem image={entelect2} script={"Entelect"} />
-          <ImageItem image={tonic2} script={"Tonic"} />
-        </div>
-        <div className="text-center mt-12">
-          <Footer />
-        </div>
+        <div></div>
       </div>
     </div>
   );
--- sap-portfolio/src/components/ImageItem.js
+++ sap-portfolio/src/components/ImageItem.js
@@ -1,19 +1,3 @@
-const ImageItem = ({ script, image }) => {
-  return (
-    <div>
-      <div className="relative">
-        <img
-          className="w-full h-80 object-cover"
-          src={image}
-          alt={`${script}-image`}
-        />
-        <div className="bg-black w-full h-80 absolute top-0 left-0 opacity-30"></div>
-        <div className="absolute bottom-4 left-4">
-          <p className="text-white font-bold text-2xl">{script}</p>
-        </div>
-      </div>
-    </div>
-  );
-};
+const ImageItem = () => {};
 
 export default ImageItem;
