6af48e766f41b0c1c9fcdf1a33918862e9f6884b
msnkr <mikyle.designs@gmail.com>
2024-12-04 09:00:34 +0200

    .

--- sap-portfolio/src/App.js
+++ sap-portfolio/src/App.js
@@ -1,22 +1,4 @@
 import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import VideoPlayer from "./components/VideoPlayer";
-
-import bos1 from "./images/home-page-image/bos-1.jpeg";
-import bos2 from "./images/home-page-image/bos-2.png";
-import newPlanet1 from "./images/home-page-image/new-planet-1.png";
-import newPlanet2 from "./images/home-page-image/new-planet-2.png";
-import newPlanet3 from "./images/home-page-image/new-planet-3.png";
-import newBeing1 from "./images/home-page-image/new-being-1.png";
-import newBeing2 from "./images/home-page-image/new-being-2.png";
-import tonic1 from "./images/home-page-image/tonic-1.png";
-import tonic2 from "./images/home-page-image/tonic-2.png";
-import csa1 from "./images/home-page-image/csa-1.png";
-import aynjil1 from "./images/home-page-image/aynjil-1.png";
-import entelect1 from "./images/home-page-image/entelect-1.png";
-import entelect2 from "./images/home-page-image/entelect-2.png";
-import tonicVideo from "./images/home-page-image/Tonic_social_28_July_1x1.mp4";
-import heyHalfwayVideo from "./images/home-page-image/HH_Stache It (4x5).mp4";
 
 export default function App() {
   return (
