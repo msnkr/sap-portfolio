6224cd25c68376bf9ec5ebb06d1b2504abc9f506
msnkr <mikyle.designs@gmail.com>
2024-12-05 12:26:41 +0200

    .

--- sap-portfolio/src/components/ImageComponent.js
+++ sap-portfolio/src/components/ImageComponent.js
@@ -4,6 +4,7 @@ const ImageComponent = ({ image }) => {
       <img
         src={image}
         className="w-full h-56 object-cover md:h-72 lg:h-[500px]"
+        alt="image-brand"
       />
     </div>
   );
